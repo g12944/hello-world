e7a66cfa3ef7fbf638163274daec118800a26d89
g12945ss <g12945ss@s30424.tsuda.ac.jp>
2015-04-10 11:21:06 +0900

    HolloWorldです

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g12945ss
+ */
+public class Helloworld {
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+}
