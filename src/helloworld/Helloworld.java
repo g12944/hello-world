7cd0d469488910b111fd3779fe6ae0899eda66a6
g12944re <g12944re@s30423.tsuda.ac.jp>
2015-04-10 11:51:08 +0900

    HelloWorld!!

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -17,5 +17,6 @@ public class Helloworld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.err.println("Shino");
     }
 }
